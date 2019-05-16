1e1ca4750082c477b481a522a221c61874816939
Dmitry Savintsev <dsavints@verizonmedia.com>
2019-05-16 21:49:30 +0200

    current state

--- exercises/functions/functions1.rs
+++ exercises/functions/functions1.rs
@@ -1,4 +1,4 @@
-// functions1.rs
+ff// functions1.rs
 // Make me compile! Execute `rustlings hint functions1` for hints :)
 
 // I AM NOT DONE
@@ -6,3 +6,42 @@
 fn main() {
     call_me();
 }
+
+fn call_me() {}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// This main function is calling a function that it expects to exist, but the
+// function doesn't exist. It expects this function to have the name `call_me`.
+// It expects this function to not take any arguments and not return a value.
+// Sounds a lot like `main`, doesn't it?
--- exercises/primitive_types/primitive_types3.rs
+++ exercises/primitive_types/primitive_types3.rs
@@ -1,11 +1,9 @@
 // primitive_types3.rs
-// Create an array with at least 100 elements in it where the ??? is. 
-// Execute `rustlings hint primitive_types3` for hints!
-
-// I AM NOT DONE
+// Create an array with at least 100 elements in it where the ??? is.
+// Scroll down for hints!
 
 fn main() {
-    let a = ???
+    let a = [1,2,3];
 
     if a.len() >= 100 {
         println!("Wow, that's a big array!");
@@ -13,3 +11,37 @@ fn main() {
         println!("Meh, I eat arrays like that for breakfast.");
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// There's a shorthand to initialize Arrays with a certain size that does not
+// require you to type in 100 items (but you certainly can if you want!).
+// For example, you can do:
+// let array = ["Are we there yet?"; 10];
+
+// Bonus: what are some other things you could have that would return true
+// for `a.len() >= 100`?

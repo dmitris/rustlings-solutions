a1171d25a7a5e935cddc5d4d9511b4e6774a2178
Dmitry Savintsev <dsavints@verizonmedia.com>
2020-04-15 12:45:18 +0200

    current state, rebased on master

--- exercises/error_handling/option1.rs
+++ exercises/error_handling/option1.rs
@@ -4,8 +4,6 @@
 // on `None`. Handle this in a more graceful way than calling `unwrap`!
 // Execute `rustlings hint option1` for hints :)
 
-// I AM NOT DONE
-
 pub fn pop_too_much() -> bool {
     let mut list = vec![3];
 

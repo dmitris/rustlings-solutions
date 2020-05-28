9319d8c2fe219b945ea671e3873a650a7fa622e5
Dmitry Savintsev <dsavints@verizonmedia.com>
2020-05-28 12:19:20 +0200

    exercises before try_from_into.rs

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -18,7 +18,6 @@ impl Default for Person {
     }
 }
 
-// I AM NOT DONE
 // Your task is to complete this implementation
 // in order for the line `let p = Person::from("Mark,20")` to compile
 // Please note that you'll need to parse the age component into a `usize`
@@ -35,6 +34,34 @@ impl Default for Person {
 // Otherwise, then return an instantiated Person object with the results
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        if s.len() == 0 {
+            return Person::default()
+        };
+        // 2. Split the given string on the commas present in it
+        let mut it = s.split(',');
+
+        // 3. Extract the first element from the split operation and use it as the name
+        // 4. If the name is empty, then return the default of Person
+        let mut name;
+        match it.next() {
+            None | Some("") => return Person::default(),
+            Some(s) => name = s.to_string()
+        }
+        let mut age;
+
+        // 5. Extract the other element from the split operation and parse it into a `usize` as the age
+        // If while parsing the age, something goes wrong, then return the default of Person
+        // Otherwise, then return an instantiated Person object with the results
+        match it.next() {
+            Some(s)  => {
+                match s.parse::<usize>() {
+                    Ok(n) => age = n,
+                    Err(_) => return Person::default()
+                }
+            },
+            None => return Person::default()
+        }
+        Person{name: name, age: age}
     }
 }
 

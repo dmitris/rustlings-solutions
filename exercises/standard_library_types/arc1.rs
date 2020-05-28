2bf8ad13033c3ca0caa7d1388333a9730ab1ed8f
Dmitry Savintsev <dsavints@verizonmedia.com>
2020-05-28 12:19:20 +0200

    exercises before try_from_into.rs

--- exercises/standard_library_types/arc1.rs
+++ exercises/standard_library_types/arc1.rs
@@ -4,7 +4,7 @@
 // somewhere. Try not to create any copies of the `numbers` Vec!
 // Execute `rustlings hint arc1` for hints :)
 
-#![forbid(unused_imports)] // Do not change this, (or the next) line.
+#![forbid(unused_imports)] // Do not change this, (or the next) line.ss
 use std::sync::Arc;
 use std::sync::Mutex;
 use std::thread;
